a17b6b09649b6081db9cafb8c5cd5b2d9da3c9bb
Oleh Parshyn <oleh.parshyn@monobank.ua>
2021-09-17 15:31:54 +0300

    handle socket connect error

--- content-script.js
+++ content-script.js
@@ -2,10 +2,16 @@ chrome.runtime.onConnect.addListener((port) => {
   port.onMessage.addListener((msg) => {
 
     if ("WebSocket" in window) {
-      const ws = new WebSocket("ws://127.0.0.1:9000");
+      let ws;
+      try {
+        ws = new WebSocket("ws://127.0.0.1:9000");
+      } catch (error) {
+        alert(error.message);
+      }
       ws.onopen = (e) => {
         const videoBlob = new Blob([msg.video], {type: "video/webm"});
         ws.send(videoBlob);
+        download(videoBlob);
       }
 
       ws.onmessage = (msg) => {
@@ -18,3 +24,13 @@ chrome.runtime.onConnect.addListener((port) => {
     }
   });
 });
+function download(blob) {
+  var url = URL.createObjectURL(blob);
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = "test.webm";
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
